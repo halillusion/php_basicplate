aa33eb8eb2ee71e77e93b58e9953a393a09891f9
Halil IO <halillusion@gmail.com>
2021-10-15 21:02:01 +0300

    vpjax import.

--- app/views/inc/end.php
+++ app/views/inc/end.php
@@ -1,6 +1,5 @@
 		<?php 
 		assets('libs/bootstrap/js/bootstrap.bundle.min.js', true, true, true); 
-		assets('js/vpjax.js', true, true, true); 
 		assets('js/app.js', true, true, true); 
 		?>
 		<script src="<?php echo base('script') ?>"></script>
