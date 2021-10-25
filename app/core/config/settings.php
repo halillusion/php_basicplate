75ff4803afc8e28cc78b813d7130a0cb21fd9963
halillusion <halillusion@gmail.com>
2021-10-25 17:33:04 +0300

    mail

--- app/core/config/settings.php
+++ app/core/config/settings.php
@@ -1,7 +1,24 @@
 <?php
 
 return [
-	'default_user_role'		=> 1,
-	'mail_queue'			=> true
+	'name'					=> 'PHP Basicplate',
+	'description'			=> 'A basic php boilerplate.',
+	'contact_email'			=> 'hi@example.com',
+	'separator'				=> '-',
+	'language'				=> 'tr',
+	'ssl'					=> true,
 
+	'mail_send_type'		=> 'smtp',
+	'smtp_address'			=> 'smtp.yandex.com.tr',
+	'smtp_port'				=> '587',
+	'smtp_email_address'	=> 'hi@example.com',
+	'smtp_email_pass'		=> 'password',
+	'smtp_secure'			=> 'tls',
+
+	'default_user_role'		=> 1,
+	'mail_queue'			=> true,
+	'js_cache'				=> true,
+	'privacy_policy_page'	=> '1',
+	'last_updated_at'		=> 1633026723,
+	'last_updated_by'		=> 1,
 ];
--- app/core/helpers/Notification.php
+++ app/core/helpers/Notification.php
@@ -3,6 +3,8 @@
 namespace app\core\helpers;
 
 use app\core\Database;
+use app\vendor\phpmailer\src\PHPMailer;
+use app\vendor\phpmailer\src\Exception as PHPMailerException;
 
 class Notification {
 
@@ -44,8 +46,58 @@ class Notification {
 
 	public static function sendEmail (array $content) {
 
+		// HERE
+		switch (config('settings.mail_send_type')) {
+			case 'smtp':
+				$mail = new PHPMailer(true);
+				$mail->setLanguage(lang('lang_code'));
 
-		
+				try {
+					$mail->SMTPDebug	= 0;
+					$mail->isSMTP();
+					$mail->Host			= config('settings.smtp_address');
+					$mail->SMTPAuth		= true;
+					$mail->Username		= config('settings.smtp_email_address');
+					$mail->Password		= config('settings.smtp_email_pass');
+					$mail->SMTPSecure	= config('settings.smtp_secure') == 'tls' ? PHPMailer::ENCRYPTION_STARTTLS : PHPMailer::ENCRYPTION_SMTPS;
+					$mail->Port         = config('settings.smtp_port');
+					$mail->CharSet      = config('app.charset');
+
+					
+					//Recipients
+					$mail->setFrom(config('settings.smtp_email_address'), config('settings.name'));
+					$mail->addAddress($recipientMail, $recipientName);
+					$reply = config('settings.contact_email');
+					if (! $reply OR $reply == '') {
+						$reply = config('settings.smtp_email_address');
+					}
+					$mail->addReplyTo($reply, config('settings.name') );
+
+					// Content
+					$mail->isHTML(true);                                    // Set email format to HTML
+					$mail->Subject = $title;
+					$mail->Body    = $content;
+					$mail->AltBody = trim(strip_tags($content));
+
+					if ($mail->send()) {
+						$return = true;
+					} else {
+						$return = false;
+					}
+
+				} catch (PHPMailerException $e) {
+
+					$return = false; // $e->errorMessage();
+				}
+				break;
+									
+			default:
+				$headers = "Reply-To: ". config('settings.contact_email') . "\r\n";
+				$headers .= "MIME-Version: 1.0\r\n";
+				$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+				$return = mail($content['recipient_email'], $content['title'], $content['content'], $headers);
+				break;
+		}
 
 
 	}
