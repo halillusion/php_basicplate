3982dcd1493f4c4b36240e6e322af1227e94af93
Halil IO <halillusion@gmail.com>
2021-10-30 03:05:24 +0300

    management controller and management pages.

---
+++ app/controllers/ManagementController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\Database;
+use app\core\helpers\Response;
+
+class ManagementController {
+
+	function __construct() {
+		
+	}
+
+	static function view ($key = null) {
+
+		global $pageStructure, $title;
+
+		$pageStructure = [
+			'inc/header',
+			'inc/management_nav',
+			'_',
+			'inc/footer',
+			'inc/end',
+		];
+
+		$title = $key;
+
+		if (is_array($pageStructure)) {
+			
+			if (isset($_SERVER['HTTP_X_VPJAX']) !== false) {
+				echo '<title>'.title(false).'</title><body id="wrap">';
+			}
+
+			foreach ($pageStructure as $part) {
+
+				if ($part == '_') { // Content File
+
+					require path('app/views/management/'.$key.'.php');
+
+				} elseif (file_exists(path('app/views/'.$part.'.php'))) { // Layout File
+
+					if (
+						isset($_SERVER['HTTP_X_VPJAX']) === false OR 
+						in_array($part, ['inc/header', 'inc/end']) === false) {
+
+						require path('app/views/'.$part.'.php');
+					}
+
+				}
+
+			}
+
+			if (isset($_SERVER['HTTP_X_VPJAX']) !== false) {
+				echo '</body>';
+			}
+			
+		}
+
+	}
+
+}
